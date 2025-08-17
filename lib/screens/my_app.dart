b787711e422dba321c2c2398d1c07abab7b18163
Nattapong Tonprasert <luciferultram@gmail.com>
2025-08-18 00:26:25 +0700

    Add custom scroll behavior to enhance drag functionality
    This commit introduces a new `AppCustomScrollBehavior` class that allows dragging with a mouse, specifically for the Continents carousel. Additionally, the `MyApp` widget is updated to utilize this custom scroll behavior, improving the user experience across the application.

--- lib/screens/my_app.dart
+++ lib/screens/my_app.dart
@@ -3,6 +3,7 @@ import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:provider/provider.dart';
 
 import '../rounting/router.dart';
+import '../ui/core/ui/scroll_behavior.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      scrollBehavior: AppCustomScrollBehavior(),
       debugShowCheckedModeBanner: false,
       routerConfig: router(context.read()),
       builder: EasyLoading.init(),
